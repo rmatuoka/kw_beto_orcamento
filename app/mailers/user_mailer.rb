8127cc52f289ab60e064afe38af6a385359747b7
Rafael Matuoka <rmatuoka@korewa.com.br>
2011-10-05 16:13:15 -0300

    Ajuste msg sucesso

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default :from => "Pesquisa Respondida - Beto Tricoli  <noreply@betotricoli.com>"
+  default :from => "sugestão de emenda  <noreply@betotricoli.com>"
   
   def resposta(survey)
     @survey = survey
-    mail(:to => "rmatuoka@korewa.com.br", :subject => "Pesquisa Orçamento 2011")
-    #mail(:to => "betotricoli@al.sp.gov.br", :bbc => "rmatuoka@korewa.com.br, betrix43@hotmail.com",  :subject => "Pesquisa Orçamento 2011")
+    #mail(:to => "rmatuoka@korewa.com.br", :subject => "Pesquisa Orçamento 2011")
+    mail(:to => "betotricoli@al.sp.gov.br", :bbc => "rmatuoka@korewa.com.br, betrix43@hotmail.com",  :subject => "Pesquisa Orçamento 2011")
   end
 end
